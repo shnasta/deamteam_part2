d59e00de2b581733b4cb09d5fb03d3418d15d479
Kolotose <amazourick@gmail.com>
2021-12-15 12:34:36 +0200

    Message for ancestors

--- generate_func.py
+++ generate_func.py
@@ -2,3 +2,4 @@ def build_board(lines):
     board = [['0' for count in range(lines)] for count in range(lines)]
     return board
 
+# Ми пробували, але ми весь час вбили на те, щоби хоча б якось розібратися з гітом
