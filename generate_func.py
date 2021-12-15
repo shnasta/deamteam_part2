671d623f0104b2cd28eba0a0611b67556ec8e0ac
shnasta <shvets.nasta@gmail.com>
2021-12-15 12:42:06 +0200

    my first commit

--- generate_func.py
+++ generate_func.py
@@ -1,2 +1,13 @@
 def generate_field():
     print("I try my best")
+def build_board(dims):
+    return [['O' for count in range(dims)] for count in range(dims)]
+def print_board(board):
+    for b in board:
+        print(*b)
+def main():
+    board = build_board(5)
+    print_board(board)
+    print('did something')
+    return
+main()
